6ff22cec9ca25edfac038d051ebed15ac1d493c7
lifeng <lifeng023872@gtjas.com>
2023-02-25 23:00:26 +0800

    NgzkHouseSite v0.2.1 可配置多个不同的refresh_token

--- api.py
+++ api.py
@@ -6,7 +6,6 @@ from urllib.parse import urlencode
 from tenacity import retry, stop_after_delay, stop_after_attempt, wait_random
 from my_config import g_config
 import socket
-
 # 为所有请求设置默认超时
 socket.setdefaulttimeout(g_config.get_config("socket_timeout", 10))
 
@@ -16,16 +15,18 @@ class NgzkMsgGroup:
     Name: str
     Id: int
     Avatar: str
+    AvailableTokenDict: dict
 
     def __init__(self, name, id, avatar) -> None:
         self.Name = name
         self.Id = id
         self.Avatar = avatar
+        self.AvailableTokenDict = {}
 
 
 class NgzkMsgApi(object):
-    refresh_token: str
-    token: str
+    refresh_token: dict
+    token: dict
     qry_timeout: int
     qry_history_spread: int
     qry_history_cnt: int
@@ -33,13 +34,12 @@ class NgzkMsgApi(object):
     g_sublist: dict
 
     def __init__(self):
-        self.refresh_token = g_config.get_config("refresh_token", '')
+        self.refresh_token = g_config.get_config("refresh_token", [])
         self.qry_timeout = g_config.get_config("qry_timeout", 3)
         self.qry_history_spread = g_config.get_config("qry_history_spread", 24)
         self.qry_history_cnt = g_config.get_config("qry_history_cnt", 200)
         self.qry_order = g_config.get_config("qry_order", 'desc')
-        print(f"refresh_token is {self.refresh_token }")
-        self.token = ''
+        self.token = {}   # key为  refresh_token   val 为 临时token
         self.g_sublist = {}
         self.get_access_token()
         self.get_subscribed_members()
@@ -47,62 +47,85 @@ class NgzkMsgApi(object):
     # 获取新的 access token
     @retry(wait=wait_random(min=2, max=5), stop=stop_after_attempt(3), reraise=True)
     def get_access_token(self):
-        payload = {"refresh_token": self.refresh_token}
-        headers = {
-            'Accept': '*/*',
-            'Accept-Encoding': 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
-            'Accept-Language': 'zh-Hans-CN;q=1.0, en-CN;q=0.9, ja-CN;q=0.8',
-            'Connection': 'keep-alive',
-            'Content-Length': '56',
-            'Host': 'api.n46.glastonr.net',
-            'User-Agent': 'Hot/1.0.03 (jp.co.sonymusic.communication.nogizaka; build:117; iOS 15.3.1) Alamofire/5.5.0',
-            'X-Talk-App-ID': 'jp.co.sonymusic.communication.nogizaka 2.2',
-            'Content-Type': 'application/json'
-        }
 
-        resp = requests.request('POST', "https://api.n46.glastonr.net/v2/update_token", data=json.dumps(payload),
-                                headers=headers, timeout=self.qry_timeout)
+        for refresh_token in self.refresh_token:
 
-        responseObject = json.loads(resp.text)
-        token = responseObject['access_token']
-        self.token = token
-        return token
+            payload = {"refresh_token": refresh_token}
+            headers = {
+                'Accept': '*/*',
+                'Accept-Encoding': 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
+                'Accept-Language': 'zh-Hans-CN;q=1.0, en-CN;q=0.9, ja-CN;q=0.8',
+                'Connection': 'keep-alive',
+                'Content-Length': '56',
+                'Host': 'api.n46.glastonr.net',
+                'User-Agent': 'Hot/1.0.03 (jp.co.sonymusic.communication.nogizaka; build:117; iOS 15.3.1) Alamofire/5.5.0',
+                'X-Talk-App-ID': 'jp.co.sonymusic.communication.nogizaka 2.2',
+                'Content-Type': 'application/json'
+            }
+
+            resp = requests.request('POST', "https://api.n46.glastonr.net/v2/update_token", data=json.dumps(payload),
+                                    headers=headers, timeout=self.qry_timeout)
+
+            responseObject = json.loads(resp.text)
+            if 'code' in responseObject and responseObject['code'] == 'unauthorized':
+                continue
+            token = responseObject['access_token']
+            self.token[refresh_token] = token
 
     @retry(wait=wait_random(min=2, max=5), stop=stop_after_attempt(3), reraise=True)
     def get_subscribed_members(self) -> dict:
         res = {}
-        payload = {"refresh_token": self.refresh_token}
-        headers = {
-            'Accept': '*/*',
-            'Accept-Encoding': 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
-            'Accept-Language': 'zh-Hans-CN;q=1.0, en-CN;q=0.9, ja-CN;q=0.8',
-            'Connection': 'keep-alive',
-            'Content-Length': '56',
-            'Host': 'api.n46.glastonr.net',
-            'Authorization': 'Bearer ' + self.token,
-            'User-Agent': 'Hot/1.0.03 (jp.co.sonymusic.communication.nogizaka; build:117; iOS 15.3.1) Alamofire/5.5.0',
-            'X-Talk-App-ID': 'jp.co.sonymusic.communication.nogizaka 2.2',
-            'Content-Type': 'application/json'
-        }
-        resp = requests.request('GET', "https://api.n46.glastonr.net/v2/groups", data=json.dumps(payload),
-                                headers=headers,
-                                timeout=self.qry_timeout)
+        for refresh_token in self.refresh_token:
 
-        responseObject = json.loads(resp.text)
-        # print(responseObject)
-        print(f'req groups: {responseObject}')
-        for item in responseObject:
-            if 'subscription' in item:
-                if 'state' in item['subscription'] and (
-                        item['subscription']['state'] == 'active' or item['subscription']['state'] == 'expired'):
-                    res[item['name']] = NgzkMsgGroup(name=item['name'], id=item['id'], avatar=item['thumbnail'])
-        # if item['name'] not in g_msg_signature:
-        #     g_msg_signature[item['name']] = []
-        self.g_sublist = res
+
+            payload = {"refresh_token": refresh_token}
+            headers = {
+                'Accept': '*/*',
+                'Accept-Encoding': 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
+                'Accept-Language': 'zh-Hans-CN;q=1.0, en-CN;q=0.9, ja-CN;q=0.8',
+                'Connection': 'keep-alive',
+                'Content-Length': '56',
+                'Host': 'api.n46.glastonr.net',
+                'Authorization': 'Bearer ' + self.token[refresh_token],
+                'User-Agent': 'Hot/1.0.03 (jp.co.sonymusic.communication.nogizaka; build:117; iOS 15.3.1) Alamofire/5.5.0',
+                'X-Talk-App-ID': 'jp.co.sonymusic.communication.nogizaka 2.2',
+                'Content-Type': 'application/json'
+            }
+            resp = requests.request('GET', "https://api.n46.glastonr.net/v2/groups", data=json.dumps(payload),
+                                    headers=headers,
+                                    timeout=self.qry_timeout)
+
+            responseObject = json.loads(resp.text)
+            # print(responseObject)
+            print(f'req groups: {responseObject}')
+            for item in responseObject:
+                if 'subscription' in item:
+                    if 'state' in item['subscription'] and (
+                            item['subscription']['state'] == 'active'):
+                        if item['name'] not in res:
+                            res[item['name']] = NgzkMsgGroup(name=item['name'], id=item['id'], avatar=item['thumbnail'])
+                        if self.token[refresh_token] not in res[item['name']].AvailableTokenDict:
+                            res[item['name']].AvailableTokenDict[refresh_token] = self.token[refresh_token]
+            # if item['name'] not in g_msg_signature:
+            #     g_msg_signature[item['name']] = []
+            self.g_sublist = res
         return res
+
+
+    def get_available_token(self, member: str):
+        if member in self.g_sublist:
+            return list(self.g_sublist[member].AvailableTokenDict.items())[0]
+
+        return None
 
     @retry(wait=wait_random(min=2, max=5), stop=stop_after_attempt(3), reraise=True)
     def get_latest_msgs(self, member: str, updated_from=None):
+
+        refresh_token, token = self.get_available_token(member)
+
+        if refresh_token is None:
+            print(f"failed to find available token for {member}")
+            return
 
         if updated_from is None:
             now_time = datetime.now()
@@ -111,7 +134,7 @@ class NgzkMsgApi(object):
             updated_from = utc_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
         res = []
-        payload = {"refresh_token": self.refresh_token}
+        payload = {"refresh_token": refresh_token}
         if member in self.g_sublist:
             headers = {
                 'Accept': '*/*',
@@ -120,7 +143,7 @@ class NgzkMsgApi(object):
                 'Connection': 'keep-alive',
                 'Content-Length': '56',
                 'Host': 'api.n46.glastonr.net',
-                'Authorization': 'Bearer ' + self.token,
+                'Authorization': 'Bearer ' + token,
                 'User-Agent': 'Hot/1.0.03 (jp.co.sonymusic.communication.nogizaka; build:117; iOS 15.3.1) Alamofire/5.5.0',
                 'X-Talk-App-ID': 'jp.co.sonymusic.communication.nogizaka 2.2',
                 'Content-Type': 'application/json'
