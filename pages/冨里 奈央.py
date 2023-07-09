3820db14578a77634dcde9b17a3505615fc72e4e
FelixLee1995 <lf729263226@outlook.com>
2023-07-09 01:03:29 +0000

    增加成员

---
+++ pages/冨里 奈央.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from api import g_msg_api
+from message_render import render_msgs
+
+########################################################
+#  每个msg页面只有成员名称不一样，  因为streamlit无法动态生成页面#
+g_member = '冨里 奈央'
+########################################################
+
+st.title(g_member)
+msgs = g_msg_api.get_latest_msgs(g_member)
+render_msgs(g_member, msgs)
